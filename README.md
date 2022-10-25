8b16a4d7a3def7dc5a0815ed7656554a19ddb7da
Miguel Angel Diaz D <migerodd888@gmail.com>
2022-10-25 16:19:01 -0500

    añadi las barras laterales

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Proyecto-JavaScript
+
+este proyecto consiste en una pagina web de compras
--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("hellow, world")
+const dMenu = document.querySelector(".navbar-email");
+const DesktopMenu = document.querySelector(".desktop-menu");
+const mMenu = document.querySelector("img.menu");
+const MobileMenu = document.querySelector(".mobile-menu");
+const cMenu = document.querySelector(".imagenCarrito");
+const carritoMenu = document.querySelector(".my-order-content");
+
+dMenu.addEventListener("click", togglelogo);
+mMenu.addEventListener("click", togglemenu);
+cMenu.addEventListener("click", togglecarrito);
+// con esta funcion yo logre hacer que al darle click se abra
+// o se cierre la barra lateral
+function togglelogo () {
+    DesktopMenu.classList.toggle("inactive");
+}
+
+function togglemenu () {
+    MobileMenu.classList.toggle("inactive");
+}
+
+function togglecarrito () {
+    carritoMenu.classList.toggle("inactive");
+}
